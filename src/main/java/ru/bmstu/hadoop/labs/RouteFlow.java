8bb9b05e025d3bcd3fc332c069aad66923b4f6ca
ramagf <ramagf@yandex.ru>
2021-12-10 18:38:10 +0300

    Scripted auto-commit on change (2021-12-10 18:38:10) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/labs/RouteFlow.java
+++ src/main/java/ru/bmstu/hadoop/labs/RouteFlow.java
@@ -1,4 +1,23 @@
 package ru.bmstu.hadoop.labs;
 
+import akka.NotUsed;
+import akka.actor.ActorSystem;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
+
 public class RouteFlow {
+    private ActorSystem system;
+    private ActorMaterializer materializer;
+
+    public RouteFlow(ActorSystem system, ActorMaterializer materializer) {
+        this.system = system;
+        this.materializer = materializer;
+    }
+
+    public Flow<HttpRequest, HttpResponse, NotUsed> createFlow() {
+        
+    }
+
 }
--- src/main/java/ru/bmstu/hadoop/labs/StreamApp.java
+++ src/main/java/ru/bmstu/hadoop/labs/StreamApp.java
@@ -1,25 +1,17 @@
 package ru.bmstu.hadoop.labs;
 
 import akka.NotUsed;
-import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
-import akka.actor.Props;
 import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.ServerBinding;
-import akka.http.javadsl.marshallers.jackson.Jackson;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
-import akka.http.javadsl.server.Route;
-import akka.pattern.Patterns;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
-import scala.concurrent.Future;
 
 import java.io.IOException;
 import java.util.concurrent.CompletionStage;
-
-import static akka.http.javadsl.server.Directives.*;
 import static ru.bmstu.hadoop.labs.Constants.*;
 
 public class StreamApp {
@@ -29,8 +21,8 @@ public class StreamApp {
 
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
-        RouteFlow routeFlow = new RouteFlow(system, materializer);
-        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = RouteFlow.createRoute().flow(system, materializer);
+        RouteFlow route = new RouteFlow(system, materializer);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = route.createFlow();
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(
                 routeFlow,
                 ConnectHttp.toHost("localhost", DEFAULT_PORT),
