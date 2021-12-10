9838dafae65d1aaf17d78ebbb06efa6dd59a60aa
ramagf <ramagf@yandex.ru>
2021-12-10 17:20:00 +0300

    Scripted auto-commit on change (2021-12-10 17:20:00) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/labs/StreamApp.java
+++ src/main/java/ru/bmstu/hadoop/labs/StreamApp.java
@@ -13,22 +13,25 @@ import java.io.IOException;
 import java.util.concurrent.CompletionStage;
 
 public class StreamApp {
+
+    
+
     public static void main(String[] args) throws IOException {
-        System.out.println("start!");
-        ActorSystem system = ActorSystem.create("routes");
+        ActorSystem system = ActorSystem.create("Routes");
+
         final Http http = Http.get(system);
-        final ActorMaterializer materializer =
-                ActorMaterializer.create(system);
-        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = <вызов
-        метода которому передаем Http, ActorSystem и ActorMaterializer>;
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+        AkkaApp instance = new AkkaApp();
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
+                instance.createRoute().flow(system, materializer);
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(
                 routeFlow,
-                ConnectHttp.toHost("localhost", 8080),
+                ConnectHttp.toHost("localhost", DEFAULT_PORT),
                 materializer
         );
-        System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
+        System.out.println(SERVER_ONLINE + DEFAULT_PORT);
         System.in.read();
-        binding
-                .thenCompose(ServerBinding::unbind)
+        binding.thenCompose(ServerBinding::unbind)
                 .thenAccept(unbound -> system.terminate());
+    }
 }
