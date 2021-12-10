428ed426480015d61025b27619817f2edf3731a4
ramagf <ramagf@yandex.ru>
2021-12-10 17:23:29 +0300

    Scripted auto-commit on change (2021-12-10 17:23:29) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/labs/StreamApp.java
+++ src/main/java/ru/bmstu/hadoop/labs/StreamApp.java
@@ -1,19 +1,25 @@
 package ru.bmstu.hadoop.labs;
 
 import akka.NotUsed;
+import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
+import akka.actor.Props;
 import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.marshallers.jackson.Jackson;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.http.javadsl.server.Route;
+import akka.pattern.Patterns;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 import scala.concurrent.Future;
 
 import java.io.IOException;
 import java.util.concurrent.CompletionStage;
+
+import static akka.http.javadsl.server.Directives.*;
 import static ru.bmstu.hadoop.labs.Constants.*;
 
 public class StreamApp {
