25aa69afb1cd99775c710b3cf606b036c2e0340e
ramagf <ramagf@yandex.ru>
2021-12-10 17:18:19 +0300

    Scripted auto-commit on change (2021-12-10 17:18:19) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/labs/StreamApp.java
+++ src/main/java/ru/bmstu/hadoop/labs/StreamApp.java
@@ -1,7 +1,28 @@
 package ru.bmstu.hadoop.labs;
 
+import akka.actor.ActorSystem;
+
+import java.io.IOException;
+
 public class StreamApp {
-    public static void main(String[] args) {
-        
-    }
+    public static void main(String[] args) throws IOException {
+        System.out.println("start!");
+        ActorSystem system = ActorSystem.create("routes");
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer =
+                ActorMaterializer.create(system);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = <вызов
+        метода которому передаем Http, ActorSystem и ActorMaterializer>;
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost("localhost", 8080),
+                materializer
+        );
+        System.out.println("Server online at http://localhost:8080/\nPress
+                RETURN to stop...");
+        System.in.read();
+        binding
+                .thenCompose(ServerBinding::unbind)
+                .thenAccept(unbound -> system.terminate());
+        when done
 }
