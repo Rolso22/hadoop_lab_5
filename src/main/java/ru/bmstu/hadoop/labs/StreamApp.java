0f4230c546087b4571a1307c0656bb4ebdcebebb
ramagf <ramagf@yandex.ru>
2021-12-10 17:19:23 +0300

    Scripted auto-commit on change (2021-12-10 17:19:23) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/labs/StreamApp.java
+++ src/main/java/ru/bmstu/hadoop/labs/StreamApp.java
@@ -1,8 +1,16 @@
 package ru.bmstu.hadoop.labs;
 
+import akka.NotUsed;
 import akka.actor.ActorSystem;
+import akka.http.javadsl.ConnectHttp;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.stream.ActorMaterializer;
 
 import java.io.IOException;
+import java.util.concurrent.CompletionStage;
 
 public class StreamApp {
     public static void main(String[] args) throws IOException {
@@ -18,11 +26,9 @@ public class StreamApp {
                 ConnectHttp.toHost("localhost", 8080),
                 materializer
         );
-        System.out.println("Server online at http://localhost:8080/\nPress
-                RETURN to stop...");
+        System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
         System.in.read();
         binding
                 .thenCompose(ServerBinding::unbind)
                 .thenAccept(unbound -> system.terminate());
-        when done
 }
