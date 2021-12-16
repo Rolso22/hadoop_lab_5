a64f025182ccdd0d27ffda42b404fb4011cffb4a
ramagf <ramagf@yandex.ru>
2021-12-16 23:51:32 +0300

    Scripted auto-commit on change (2021-12-16 23:51:32) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/labs/CacheActor.java
+++ src/main/java/ru/bmstu/hadoop/labs/CacheActor.java
@@ -2,15 +2,22 @@ package ru.bmstu.hadoop.labs;
 
 import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
+import akka.http.javadsl.model.HttpRequest;
 import akka.japi.pf.ReceiveBuilder;
 import java.util.HashMap;
 
-public class ActorSystem extends AbstractActor {
+public class CacheActor extends AbstractActor {
+    
 
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create()
-                //.match()
+                .match(HttpRequest.class, this::getResult)
                 .build();
     }
+
+    private void getResult(HttpRequest request) {
+
+    }
+
 }
