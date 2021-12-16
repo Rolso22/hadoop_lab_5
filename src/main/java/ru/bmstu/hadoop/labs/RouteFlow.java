3a7bd464a7dd0d7e8d8b50747b2870c18247fc6f
ramagf <ramagf@yandex.ru>
2021-12-17 00:08:50 +0300

    Scripted auto-commit on change (2021-12-17 00:08:50) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/labs/RouteFlow.java
+++ src/main/java/ru/bmstu/hadoop/labs/RouteFlow.java
@@ -38,7 +38,7 @@ public class RouteFlow {
                 .mapAsync(2, request -> {
                     CompletionStage<Object> result = Patterns.ask(cacheActor, new CacheMessage(request.first(), request.second()), Duration.ofMillis(TIME_OUT_MILLIS));
                     result.thenCompose(answer -> {
-                        
+                        if ()
                     })
                 });
     }
