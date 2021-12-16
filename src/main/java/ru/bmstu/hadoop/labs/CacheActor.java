d745ecc757025c82f630a4afe0a55541662c42d4
ramagf <ramagf@yandex.ru>
2021-12-16 23:58:27 +0300

    Scripted auto-commit on change (2021-12-16 23:58:27) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/labs/CacheActor.java
+++ src/main/java/ru/bmstu/hadoop/labs/CacheActor.java
@@ -4,9 +4,6 @@ import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
 import akka.http.javadsl.model.HttpRequest;
 import akka.japi.pf.ReceiveBuilder;
-import com.sun.xml.internal.ws.util.CompletedFuture;
-
-
 import java.util.HashMap;
 
 public class CacheActor extends AbstractActor {
@@ -22,7 +19,6 @@ public class CacheActor extends AbstractActor {
     private void getResult(CacheMessage request) {
         String url = request.getUrl();
         if (store.containsKey(url)) {
-            CompletedFuture<Integer> result = 
             sender().tell(store.get(url), ActorRef.noSender());
         } else {
             sender().tell("no cache", ActorRef.noSender());
