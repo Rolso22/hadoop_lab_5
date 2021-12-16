d0d3fc6d0bc5b0555d78850715e43713743596c9
ramagf <ramagf@yandex.ru>
2021-12-16 23:44:19 +0300

    Scripted auto-commit on change (2021-12-16 23:44:19) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/labs/RouteFlow.java
+++ src/main/java/ru/bmstu/hadoop/labs/RouteFlow.java
@@ -1,13 +1,17 @@
 package ru.bmstu.hadoop.labs;
 
 import akka.NotUsed;
+import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
+import akka.actor.Props;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.japi.Pair;
+import akka.pattern.Patterns;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 import akka.stream.javadsl.Sink;
+import com.sun.xml.internal.ws.util.CompletedFuture;
 
 import java.util.concurrent.CompletionStage;
 import static ru.bmstu.hadoop.labs.Constants.*;
@@ -15,10 +19,12 @@ import static ru.bmstu.hadoop.labs.Constants.*;
 public class RouteFlow {
     private ActorSystem system;
     private ActorMaterializer materializer;
+    private ActorRef cacheActor;
 
     public RouteFlow(ActorSystem system, ActorMaterializer materializer) {
         this.system = system;
         this.materializer = materializer;
+        cacheActor = system.actorOf(Props.create(ActorSystem.class));
     }
 
     public Flow<HttpRequest, HttpResponse, NotUsed> createFlow() {
@@ -28,7 +34,7 @@ public class RouteFlow {
                             Integer.parseInt(request.getUri().query().get(TEST_COUNT).get()));
                 })
                 .mapAsync(2, request -> {
-
+                    CompletedFuture<String> result = Patterns.ask(cacheActor, )
                 });
     }
 
